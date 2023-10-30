d1a4909e0fae37ffe8f37edd07e165aa5232f2f3
flytreeleft <flytreeleft@crazydan.org>
2023-10-30 23:21:07 +0800

    改进对 X 型键盘的输入字符的布局机制

--- app/src/main/java/org/crazydan/studio/app/ime/kuaizi/internal/view/XKeyboardView.java
+++ app/src/main/java/org/crazydan/studio/app/ime/kuaizi/internal/view/XKeyboardView.java
@@ -46,29 +46,32 @@ public class XKeyboardView extends View {
     private static final short TRAIL_STEPS = 100;
     private static final byte TRAIL_STEP_DISTANCE = 5;
     private static final byte TRAIL_MAX_RADIUS = 8;
+    private static final float cos_30 = (float) Math.cos(Math.toRadians(30));
 
     private final HexagonOrientation orientation = HexagonOrientation.FLAT_TOP;
     private final Paint paint;
     private final Path path;
     private final PointF[][] zoneVertexes = new PointF[6][];
-    private final PointF currentPoint = new PointF();
+    private final PointF currentPoint = new PointF(-100000, -100000);
 
     private final Path trailPath = new Path();
     private final Paint trailPaint = new Paint();
     private final float[] trailPathPos = new float[2];
     private final PathMeasure trailPathMeasure = new PathMeasure();
 
-    private int hexagonRadius = 128;
+    private int hexagonRadius = 142;
     private int hexagonCornerRadius = 12;
     private String dividerStyle;
 
     public XKeyboardView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
 
-        this.dividerStyle = ThemeUtils.getStringByAttrId(context, R.attr.key_disabled_border_style);
+        this.dividerStyle = ThemeUtils.getStringByAttrId(context, R.attr.x_keyboard_divider_style);
 
-        this.paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         this.path = new Path();
+        this.paint = new Paint();
+        this.paint.setAntiAlias(true);
+        this.paint.setStrokeJoin(Paint.Join.ROUND);
 
         ViewUtils.prepareForShadow(this);
     }
@@ -78,7 +81,7 @@ public class XKeyboardView extends View {
     public boolean onTouchEvent(MotionEvent e) {
         invalidate();
 
-        this.currentPoint.set(0, 0);
+        this.currentPoint.set(-100000, -100000);
 
         // Note：需要返回 true 才能拦截到 move 等事件
         switch (e.getActionMasked()) {
@@ -118,7 +121,9 @@ public class XKeyboardView extends View {
         int height = getHeight();
         PointF center = new PointF(width / 2f, height / 2f);
         float innerHexagonRadius = this.hexagonRadius;
-        float outerHexagonRadius = Math.min(width, height) / 2f;
+        float outerHexagonRadius = Math.min(width, height) / (2f * (this.orientation == HexagonOrientation.FLAT_TOP
+                                                                    ? cos_30
+                                                                    : 1));
         float circleRadius = this.hexagonRadius * 0.4f;
         int keyNormalColor = ThemeUtils.getColorByAttrId(getContext(), R.attr.key_fg_color);
         int keyHighlightColor = ThemeUtils.getColorByAttrId(getContext(), R.attr.key_highlight_fg_color);
@@ -145,15 +150,21 @@ public class XKeyboardView extends View {
         }
         canvas.drawPath(this.path, this.paint);
 
+//        // 绘制外边界
+//        this.path.reset();
+//        this.path.addRect(0, 0, width, height, Path.Direction.CW);
+//        canvas.drawPath(this.path, this.paint);
+
         // 查找激活的分区
         int activeZoneIndex = -1;
+        PointF[] maxBoundHexagonVertexes = ViewUtils.createHexagon(this.orientation, center, outerHexagonRadius * 2);
         for (int i = 0; i < innerHexagonVertexes.length; i++) {
             int startIndex = i;
             int endIndex = (i + 1) % innerHexagonVertexes.length;
             PointF innerStart = innerHexagonVertexes[startIndex];
             PointF innerEnd = innerHexagonVertexes[endIndex];
-            PointF outerStart = outerHexagonVertexes[startIndex];
-            PointF outerEnd = outerHexagonVertexes[endIndex];
+            PointF outerStart = maxBoundHexagonVertexes[startIndex];
+            PointF outerEnd = maxBoundHexagonVertexes[endIndex];
 
             this.zoneVertexes[i] = new PointF[] { innerEnd, innerStart, outerStart, outerEnd };
 
@@ -178,9 +189,28 @@ public class XKeyboardView extends View {
         canvas.drawPath(this.path, this.paint);
 
         // 绘制输入文本
+        float keyTextSize = 45f;
+        float keyHalfTextSize = keyTextSize / 2f;
+        float keySpacing = (outerHexagonRadius - innerHexagonRadius - keyHalfTextSize) / 3f;
+//        // - 绘制输入文本布局线
+//        this.path.reset();
+//
+//        for (int i = 0; i < 4; i++) {
+//            float radius = keySpacing * (i + 1) + innerHexagonRadius;
+//
+//            this.path.reset();
+//            ViewUtils.drawHexagon(this.path,
+//                                  this.orientation == HexagonOrientation.FLAT_TOP
+//                                  ? HexagonOrientation.POINTY_TOP
+//                                  : HexagonOrientation.FLAT_TOP,
+//                                  center,
+//                                  radius);
+//            canvas.drawPath(this.path, this.paint);
+//        }
+
+        // - 绘制输入文本
         this.path.reset();
 
-        float keyTextSize = 35f;
         this.paint.setStrokeWidth(0f);
         this.paint.setStyle(Paint.Style.FILL_AND_STROKE);
         this.paint.setTextSize(keyTextSize);
@@ -190,47 +220,93 @@ public class XKeyboardView extends View {
         // - 第二维：每个分区的两条辐射轴线，沿顺时针方向，依次定为 x 轴和 y 轴
         // - 底三维：分区的 x/y 轴上的位置
         // 例如，(0, 0, 1) 表示第 0 区的 x 轴上序号为 1 的点；(1, 2, 0) 表示第 1 区 y 轴上序号为 2 的点
-        float keyHalfTextSize = keyTextSize / 2f;
-        float keySpacing = (outerHexagonRadius - innerHexagonRadius - keyHalfTextSize) / 3f;
         String[][][] keys = getKeys();
 
         for (int i = 0; i < 3; i++) {
             float radius = keySpacing * (i + 1) + innerHexagonRadius;
-            PointF[] vertexes = ViewUtils.createHexagon(this.orientation, center, radius);
+            PointF[] vertexes = ViewUtils.createHexagon(this.orientation == HexagonOrientation.FLAT_TOP
+                                                        ? HexagonOrientation.POINTY_TOP
+                                                        : HexagonOrientation.FLAT_TOP, center, radius);
 
             for (int j = 0; j < vertexes.length; j++) {
-                PointF start = vertexes[j]; // 分区 x 轴上的点
-                PointF end = vertexes[(j + 1) % vertexes.length]; // 分区 y 轴上的点
-                String xKey = keys[j][j > 2 ? 1 : 0][i];
-                String yKey = keys[j][j > 2 ? 0 : 1][i];
+                PointF before = vertexes[(j > 0 ? j : vertexes.length) - 1];
+                PointF current = vertexes[j];
+                PointF after = vertexes[(j + 1) % vertexes.length];
+                PointF xPoint = new PointF((current.x + before.x) / 2f, (current.y + before.y) / 2f);
+                PointF yPoint = new PointF((current.x + after.x) / 2f, (current.y + after.y) / 2f);
 
-                this.path.reset();
-                // 在水平线上部，需在外圈绘制文本（绘制线向量沿顺时针方向），以确保文字直立显示
-                if (j > 2) {
-                    this.path.moveTo(start.x, start.y);
-                    this.path.lineTo(end.x, end.y);
-                }
-                // 在水平线下部，需在内圈绘制文本（绘制线向量沿逆时针方向），以确保文字直立显示
-                else {
-                    this.path.moveTo(end.x, end.y);
-                    this.path.lineTo(start.x, start.y);
-                }
-//                // 文本对齐的辅助线
-//                canvas.drawPath(this.path, this.paint);
+                String xKey = keys[j][0][i];
+                String yKey = keys[j][1][i];
 
                 this.paint.setColor(j == activeZoneIndex ? keyHighlightColor : keyNormalColor);
 
-                // 靠近分区 x 轴绘制文本：文本沿向量方向右对齐
-                this.paint.setTextAlign(Paint.Align.RIGHT);
-                canvas.drawTextOnPath(xKey, this.path, -keyHalfTextSize, j > 2 ? keyHalfTextSize : 0, this.paint);
+                float hOffset;
+                float vOffset;
 
-                // 靠近分区 y 轴绘制文本：文本沿向量方向左对齐
-                this.paint.setTextAlign(Paint.Align.LEFT);
-                canvas.drawTextOnPath(yKey, this.path, keyHalfTextSize, j > 2 ? keyHalfTextSize : 0, this.paint);
+                // 靠近分区 x 轴绘制文本
+                this.path.reset();
+                if (j == 0) {
+                    this.path.moveTo(center.x, center.y);
+                    this.path.lineTo(xPoint.x, xPoint.y);
+                    this.paint.setTextAlign(Paint.Align.RIGHT);
+                    hOffset = keyHalfTextSize;
+                    vOffset = keyTextSize;
+                } else if (j == 3) {
+                    this.path.moveTo(xPoint.x, xPoint.y);
+                    this.path.lineTo(center.x, center.y);
+                    this.paint.setTextAlign(Paint.Align.LEFT);
+                    hOffset = -keyHalfTextSize;
+                    vOffset = -keyHalfTextSize;
+                } else if (j <= 2) {
+                    this.path.moveTo(current.x, current.y);
+                    this.path.lineTo(xPoint.x, xPoint.y);
+                    this.paint.setTextAlign(Paint.Align.RIGHT);
+                    hOffset = -keyHalfTextSize;
+                    vOffset = keyHalfTextSize / 2f;
+                } else {
+                    this.path.moveTo(xPoint.x, xPoint.y);
+                    this.path.lineTo(current.x, current.y);
+                    this.paint.setTextAlign(Paint.Align.LEFT);
+                    hOffset = keyHalfTextSize;
+                    vOffset = keyHalfTextSize / 2f;
+                }
+                canvas.drawTextOnPath(xKey, this.path, hOffset, vOffset, this.paint);
+
+                // 靠近分区 y 轴绘制文本
+                this.path.reset();
+                if (j == 2) {
+                    this.path.moveTo(yPoint.x, yPoint.y);
+                    this.path.lineTo(center.x, center.y);
+                    this.paint.setTextAlign(Paint.Align.LEFT);
+                    hOffset = -keyHalfTextSize;
+                    vOffset = keyTextSize;
+                } else if (j == 5) {
+                    this.path.moveTo(center.x, center.y);
+                    this.path.lineTo(yPoint.x, yPoint.y);
+                    this.paint.setTextAlign(Paint.Align.RIGHT);
+                    hOffset = keyHalfTextSize;
+                    vOffset = -keyHalfTextSize;
+                } else if (j <= 2) {
+                    this.path.moveTo(yPoint.x, yPoint.y);
+                    this.path.lineTo(current.x, current.y);
+                    this.paint.setTextAlign(Paint.Align.LEFT);
+                    hOffset = keyHalfTextSize;
+                    vOffset = keyHalfTextSize / 2f;
+                } else {
+                    this.path.moveTo(current.x, current.y);
+                    this.path.lineTo(yPoint.x, yPoint.y);
+                    this.paint.setTextAlign(Paint.Align.RIGHT);
+                    hOffset = -keyHalfTextSize;
+                    vOffset = keyHalfTextSize / 2f;
+                }
+                canvas.drawTextOnPath(yKey, this.path, hOffset, vOffset, this.paint);
             }
         }
 
         // 绘制按钮文本
+        float labelTextSize = 30f;
+        this.paint.setTextSize(labelTextSize);
+
         String[] switcherTexts = new String[] { "英文", "拼音", "数字", "算数", "", "大写" };
         for (int i = 0; i < innerHexagonVertexes.length; i++) {
             PointF start = innerHexagonVertexes[i]; // 分区 x 轴上的点
@@ -251,7 +327,11 @@ public class XKeyboardView extends View {
             this.paint.setColor(keyNormalColor);
             this.paint.setTextAlign(Paint.Align.CENTER);
 
-            canvas.drawTextOnPath(switcherTexts[i], this.path, 0, i > 2 ? keyTextSize : -10, this.paint);
+            canvas.drawTextOnPath(switcherTexts[i],
+                                  this.path,
+                                  0,
+                                  i > 2 ? labelTextSize * 1.5f : -labelTextSize * 0.7f,
+                                  this.paint);
         }
     }
 
@@ -279,7 +359,7 @@ public class XKeyboardView extends View {
     private String[][][] getKeys() {
         return new String[][][] {
                 new String[][] {
-                        new String[] { "r", "g", "" }, new String[] { "Space", "", "" },
+                        new String[] { "i", "u", "ü" }, new String[] { "", "Space", "" },
                         }, //
                 new String[][] {
                         new String[] { "", "", "" }, new String[] { "p", "w", "y" },
@@ -288,13 +368,13 @@ public class XKeyboardView extends View {
                         new String[] { "a", "e", "o" }, new String[] { "h", "k", "t" },
                         }, //
                 new String[][] {
-                        new String[] { "i", "u", "ü" }, new String[] { "d", "b", "f" },
+                        new String[] { "n", "l", "m" }, new String[] { "d", "b", "f" },
                         }, //
                 new String[][] {
                         new String[] { "j", "q", "x" }, new String[] { "z", "c", "s" },
                         }, //
                 new String[][] {
-                        new String[] { "zh", "ch", "sh" }, new String[] { "n", "l", "m" },
+                        new String[] { "zh", "ch", "sh" }, new String[] { "", "r", "g" },
                         },
                 };
     }
