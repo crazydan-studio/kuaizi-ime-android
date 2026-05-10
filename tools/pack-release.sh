6398a5ac61246cc64574a6af9dcb0657b2b3205a
flytreeleft <flytreeleft@crazydan.org>
2026-05-10 17:49:38 +0800

    调整构建脚本位置

--- tools/pack-release.sh
+++ tools/pack-release.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-_DIR_="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+_DIR_="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && pwd -P)"
 
 pushd "${_DIR_}"
     JAVA_HOME=/usr/lib/jvm/java-17-openjdk \
